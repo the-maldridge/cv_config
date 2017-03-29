fbf7f7b7390dc2e7a35f5ad7d2ad01b59a1e4620
Michael Aldridge <aldridge.mac@gmail.com>
2017-03-28 22:38:01 -0500

    Clean up the doc_site build process

---
+++ roles/doc_site/templates/mkdocs_build.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+MKDOCS_SITE_PATH="/opt/var/mkdocs/{{ mkdocs_site_name }}"
+MKDOCS_CLONE_URI="{{ mkdocs_source_url }}"
+MKDOCS_SERVE_PATH="{{ mkdocs_site_path}}"
+
+# Does the Mkdocs content path exist
+if [ ! -d "$MKDOCS_SITE_PATH" ] ; then
+    mkdir -p "$MKDOCS_SITE_PATH"
+fi
+
+# Jump into the content path
+cd "$MKDOCS_SITE_PATH"
+
+# If we have .git update, otherwise clone
+if [ -d .git ] ; then
+    git pull
+else
+    git clone "$MKDOCS_CLONE_URI" .
+fi
+
+# Build destdir if it doesn't exist
+if [ ! -d "$MKDOCS_SERVE_PATH" ] ; then
+    mkdir -p "$MKDOCS_SERVE_PATH"
+fi
+
+# Build the site
+mkdocs build
+
+# Deploy the site
+rsync -a site/ "$MKDOCS_SERVE_PATH"
+
+# Fix the system permissions
+chmod og+rX "$MKDOCS_SERVE_PATH"
